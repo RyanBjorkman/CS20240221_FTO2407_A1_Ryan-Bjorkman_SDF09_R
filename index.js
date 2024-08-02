d65de0abcec9de0fa189120f517fc5cc26f39008
Ryan Bjorkman <ryanbjorkman@icloud.com>
2024-08-02 06:37:11 +0200

    created index.css

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
-// 1. Create two variables, firstCard and secondCard. 
-// Set their values to a random number between 2-11
+let firstCard = 10
+let secondCard = 11
+let sum = firstCard + secondCard + 4
+let hasBlackJack = false
+let isAlive = true
+// 1. Declare a variable called message and assign its value to an empty string
+let message = ""
 
-// 2. Create a variable, sum, and set it to the sum of the two cards
+// 2. Reassign the message variable to the string we're logging out
+if (sum <= 20) {
+    message = "Do you want to draw a new card? 🙂"
+} else if (sum === 21) {
+    message = "Wohoo! You've got Blackjack! 🥳"
+    hasBlackJack = true
+} else {
+    message = "You're out of the game! 😭"
+    isAlive = false
+}
 
-let firstCard = 6
-let secondCard = 9
-
-let sum = firstCard + secondCard
-
-console.log(sum)
+// 3. Log it out!
+console.log(message)
