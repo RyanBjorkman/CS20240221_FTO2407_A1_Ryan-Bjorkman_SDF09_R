684ebb3f79d841c1b9f148f2855a5ec17c5e91c1
Ryan Bjorkman <ryanbjorkman@icloud.com>
2024-08-02 14:47:03 +0200

    final commit

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+let player = {
+    name: "Ryan",
+    chips: 200
+}
 let cards = []
 let sum = 0
 let hasBlackJack = false
@@ -6,6 +10,10 @@ let message = ""
 let messageEl = document.getElementById("message-el")
 let sumEl = document.getElementById("sum-el")
 let cardsEl = document.getElementById("cards-el")
+
+let playerEl = document.getElementById("player-el")
+
+playerEl.textContent = player.name + ": $" + player.chips
 
 function getRandomCard() {
     let randomNumer = Math.floor( Math.random()*13 ) + 1
